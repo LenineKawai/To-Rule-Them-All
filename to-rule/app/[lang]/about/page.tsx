c3388ebd3ae0a9da53445130a1afeb6ef44290de
LenineKawai <max.golinvaux@gmail.com>
2024-02-06 17:15:14 +0100

    fix : faulty params in about

--- to-rule/app/[lang]/about/page.tsx
+++ to-rule/app/[lang]/about/page.tsx
@@ -2,8 +2,12 @@ import React from "react";
 import { getDictionary } from "@/get-dictionary";
 import { Locale } from "@/i18n-config";
 
-export default async function About(params: { lang: Locale }) {
-  const dictionary = await getDictionary(params.lang);
+export default async function About({
+  params: { lang },
+}: {
+  params: { lang: Locale };
+}) {
+  const dictionary = await getDictionary(lang);
 
   return <> {dictionary.about.title} </>;
 }
